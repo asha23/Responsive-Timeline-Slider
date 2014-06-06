126f780ea0abfedef51727246c44b27c1511bfd8
Ashley Whiting <ashleywhiting@akauk.com>
2014-06-06 17:28:51 +0100

    changed some variable names

--- library/scripts/timeline-divs-new.js
+++ library/scripts/timeline-divs-new.js
@@ -11,22 +11,22 @@ jQuery.fn.timeline = function(options){
 		datesDiv: 					'#dates',			// value: any HTML tag or #id, default to #dates
 		datesSelectedClass: 		'selected',			// value: any class, default to selected
 		datesSpeed: 				'slow',				// value: integer between 100 and 1000 (recommended) or 'slow', 'normal' or 'fast'; default to normal
-		issuesDiv: 					'#issues',			// value: any HTML tag or #id, default to #issues
-		issuesSelectedClass: 		'selected',			// value: any class, default to selected
-		issuesSpeed: 				'medium',				// value: integer between 100 and 1000 (recommended) or 'slow', 'normal' or 'fast'; default to fast
-		issuesTransparency: 		0.2,				// value: integer between 0 and 1 (recommended), default to 0.2
-		issuesTransparencySpeed: 	500,				// value: integer between 100 and 1000 (recommended), default to 500 (normal)
+		slideDiv: 					'#issues',			// value: any HTML tag or #id, default to #issues
+		slideSelectedClass: 		'selected',			// value: any class, default to selected
+		slideSpeed: 				'medium',				// value: integer between 100 and 1000 (recommended) or 'slow', 'normal' or 'fast'; default to fast
+		slideTransparency: 		0.2,				// value: integer between 0 and 1 (recommended), default to 0.2
+		slideTransparencySpeed: 	500,				// value: integer between 100 and 1000 (recommended), default to 500 (normal)
 		startAt: 					1,					// value: integer, default to 1 (first)
 	}, options);
 
 	$(function(){
 		
 		var howManyDates = $(opts.datesDiv+' div').length;
-		var howManyIssues = $(opts.issuesDiv+' div').length;
+		var howManySlides = $(opts.slideDiv+' div').length;
 		var currentDate = $(opts.datesDiv).find('a.'+opts.datesSelectedClass);
-		var currentIssue = $(opts.issuesDiv).find('div.'+opts.issuesSelectedClass);
+		var currentIssue = $(opts.slideDiv).find('div.'+opts.slideSelectedClass);
 		var widthContainer = $(opts.containerDiv).width();
-		var widthIssues = widthContainer;
+		var widthSlide = widthContainer;
 		var widthDates = $(opts.datesDiv).width();
 		var widthDate = 100;
 		
@@ -35,7 +35,7 @@ jQuery.fn.timeline = function(options){
 
 		// set positions!
 			
-		$(opts.issuesDiv).width(widthContainer*howManyIssues);
+		$(opts.slideDiv).width(widthContainer*howManySlides);
 		$(opts.datesDiv).width(widthDate*howManyDates).css('marginLeft',widthContainer/2-widthDate/2);
 		
 		defaultPositionDates = parseInt($(opts.datesDiv).css('marginLeft').substring(0,$(opts.datesDiv).css('marginLeft').indexOf('px')));
@@ -47,10 +47,10 @@ jQuery.fn.timeline = function(options){
 			$(opts.issuesDiv).animate({
 					'marginLeft':-widthContainer*currentIndex
 				},{
-					queue:false, duration:opts.issuesSpeed
+					queue:false, duration:opts.slideSpeed
 			});
 			
-			$(opts.issuesDiv+' div').animate().removeClass(opts.issuesSelectedClass).eq(currentIndex).addClass(opts.issuesSelectedClass).fadeTo(opts.issuesTransparencySpeed,1);
+			$(opts.slideDiv+' div').animate().removeClass(opts.slideSelectedClass).eq(currentIndex).addClass(opts.slideSelectedClass).fadeTo(opts.slideTransparencySpeed,1);
 			
 			$(opts.datesDiv+' a').removeClass(opts.datesSelectedClass);
 			
