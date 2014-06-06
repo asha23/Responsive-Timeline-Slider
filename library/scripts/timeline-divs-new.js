ca91c838c46d5c6b9c31e5ed76a6df5bde025892
Ashley Whiting <ashleywhiting@akauk.com>
2014-06-06 17:37:53 +0100

    Updated script

--- library/scripts/timeline-divs-new.js
+++ library/scripts/timeline-divs-new.js
@@ -1,69 +1,74 @@
 /* ----------------------------------
-jQuery timeline version 1
+jQuery timeline version 1.2
+Author: Ash Whiting
+
+A heavily stripped-down version of Jquery Timelinr
+http://www.csslab.cl/2011/08/18/jquery-timelinr/
+
 ---------------------------------- */
 
 jQuery.fn.timeline = function(options){
 
-	// default plugin opts
-	var opts = jQuery.extend({
-		orientation: 				'horizontal',		// value: horizontal | vertical, default to horizontal
-		containerDiv: 				'#timeline',		// value: any HTML tag or #id, default to #timeline
-		datesDiv: 					'#dates',			// value: any HTML tag or #id, default to #dates
-		datesSelectedClass: 		'selected',			// value: any class, default to selected
-		datesSpeed: 				'slow',				// value: integer between 100 and 1000 (recommended) or 'slow', 'normal' or 'fast'; default to normal
-		slideDiv: 					'#issues',			// value: any HTML tag or #id, default to #issues
-		slideSelectedClass: 		'selected',			// value: any class, default to selected
-		slideSpeed: 				'medium',				// value: integer between 100 and 1000 (recommended) or 'slow', 'normal' or 'fast'; default to fast
-		slideTransparency: 		0.2,				// value: integer between 0 and 1 (recommended), default to 0.2
-		slideTransparencySpeed: 	500,				// value: integer between 100 and 1000 (recommended), default to 500 (normal)
-		startAt: 					1,					// value: integer, default to 1 (first)
+	// default plugin options
+	var options = jQuery.extend({
+		containerDiv:                        '#timeline',		
+		datesDiv:                            '#dates',			
+		datesSelectedClass:                  'selected',			
+		datesSpeed:                          'slow',				
+		slideDiv:                            '#issues',			
+		slideSelectedClass:                  'selected',			
+		slideSpeed:                          'medium',			
+		slideTransparency:                   0.2,				
+		slideTransparencySpeed:              500,				
+		startAt:                             1,
 	}, options);
 
 	$(function(){
 		
-		var howManyDates = $(opts.datesDiv+' div').length;
-		var howManySlides = $(opts.slideDiv+' div').length;
-		var currentDate = $(opts.datesDiv).find('a.'+opts.datesSelectedClass);
-		var currentIssue = $(opts.slideDiv).find('div.'+opts.slideSelectedClass);
-		var widthContainer = $(opts.containerDiv).width();
+		var howManyDates = $(options.datesDiv+' div').length;
+		var howManySlides = $(options.slideDiv+' div').length;
+		var currentDate = $(options.datesDiv).find('a.'+options.datesSelectedClass);
+		var currentSlide = $(options.slideDiv).find('div.'+options.slideSelectedClass);
+		var widthContainer = $(options.containerDiv).width();
 		var widthSlide = widthContainer;
-		var widthDates = $(opts.datesDiv).width();
+		var widthDates = $(options.datesDiv).width();
 		var widthDate = 100;
 		
 		// Dynamically set the width of the slide
+		
 		$('body').find('.historyEntry').css('width', widthContainer);
 
 		// set positions!
 			
-		$(opts.slideDiv).width(widthContainer*howManySlides);
-		$(opts.datesDiv).width(widthDate*howManyDates).css('marginLeft',widthContainer/2-widthDate/2);
+		$(options.slideDiv).width(widthContainer*howManySlides);
+		$(options.datesDiv).width(widthDate*howManyDates).css('marginLeft',widthContainer/2-widthDate/2);
 		
-		defaultPositionDates = parseInt($(opts.datesDiv).css('marginLeft').substring(0,$(opts.datesDiv).css('marginLeft').indexOf('px')));
-		$(opts.datesDiv+' a').click(function(event){
+		defaultPositionDates = parseInt($(options.datesDiv).css('marginLeft').substring(0,$(options.datesDiv).css('marginLeft').indexOf('px')));
+		$(options.datesDiv+' a').click(function(event){
 			event.preventDefault();
 
 			var currentIndex = $(this).parent().prevAll().length;
 			
-			$(opts.issuesDiv).animate({
+			$(options.issuesDiv).animate({
 					'marginLeft':-widthContainer*currentIndex
 				},{
-					queue:false, duration:opts.slideSpeed
+					queue:false, duration:options.slideSpeed
 			});
 			
-			$(opts.slideDiv+' div').animate().removeClass(opts.slideSelectedClass).eq(currentIndex).addClass(opts.slideSelectedClass).fadeTo(opts.slideTransparencySpeed,1);
+			$(options.slideDiv+' div').animate().removeClass(options.slideSelectedClass).eq(currentIndex).addClass(options.slideSelectedClass).fadeTo(options.slideTransparencySpeed,1);
 			
-			$(opts.datesDiv+' a').removeClass(opts.datesSelectedClass);
+			$(options.datesDiv+' a').removeClass(options.datesSelectedClass);
 			
-			$(this).addClass(opts.datesSelectedClass);
+			$(this).addClass(options.datesSelectedClass);
 			
-			$(opts.datesDiv).animate({
-				'marginLeft':defaultPositionDates-(widthDate*currentIndex)
+			$(options.datesDiv).animate({
+					'marginLeft':defaultPositionDates-(widthDate*currentIndex)
 				} , {
 					queue:false, 
-					duration:'opts.datesSpeed'
+					duration:'options.datesSpeed'
 				});
 			});
 
-		$(opts.datesDiv+' div').eq(opts.startAt-1).find('a').trigger('click');
+		$(options.datesDiv+' div').eq(options.startAt-1).find('a').trigger('click');
 	});
 };
