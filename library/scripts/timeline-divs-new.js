97ca6430dde307f63e909aafadbef56e14e29a24
Ashley Whiting <ashleywhiting@akauk.com>
2014-06-09 13:34:13 +0100

    Stripped the jQuery out of the view.

--- library/scripts/timeline-divs-new.js
+++ library/scripts/timeline-divs-new.js
@@ -67,4 +67,111 @@ jQuery.fn.timeline = function(options){
 
 		$(options.datesDiv+' div').eq(options.startAt-1).find('a').trigger('click');
 	});
+	
+	
+	// Client side 
+	// ******************************************************************************
+	
+	// Grab the current window size so we know what it is
+	var  windowSize = $(window).width();
+	
+	// Make these variables globals
+
+	var device = false;
+	var windowType = "";
+	var isTablet = false;
+	
+	// timeline initialisation event - boom
+	
+	function launchTimeline() {
+        $().timeline();
+	}
+	
+	function isDeviceMobile(){
+		var isMobile = {
+			Android: function() {
+				return navigator.userAgent.match(/Android/i);
+			},
+			BlackBerry: function() {
+				return navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/BB10; Touch/);
+			},
+			iOS: function() {
+				return navigator.userAgent.match(/iPhone|iPod/i);
+			},
+			iPad: function() {
+				return navigator.userAgent.match(/iPad/i);
+			},
+			Opera: function() {
+				return navigator.userAgent.match(/Opera Mini/i);
+			},
+			Windows: function() {
+				return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/webOS/i) ;
+			},
+			any: function() {
+				return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.iPad() || isMobile.Opera() || isMobile.Windows());
+			}
+		};      
+		device = isMobile.any();
+	}
+						
+	isDeviceMobile();
+		
+	// Are we a mobile device?
+	
+	if (device != false) {
+		var mobileDevice = true;
+	}
+	
+	// If not, initialise the timeline
+	
+	if (mobileDevice != true) {
+		launchTimeline();
+	}
+	
+	// Check for tablet if it's Android/Blackberry... A tad hacky this, but it works
+	
+	function checkDeviceType () {
+		if (windowSize <= 1024 && windowSize >= 769 && device=="Android" || windowSize <= 1024 && windowSize >= 601 && device=="Blackberry") {
+		
+			windowType = "tablet-landscape"; // just for reference
+			isTablet = true;
+			
+		} else if (windowSize <= 768 && device=="Android" || windowSize <= 600 && device=="Blackberry") {
+		
+			windowType = "tablet-portrait"; // just for reference
+			isTablet = true;
+		}
+	}
+	
+	// If I'm a tablet then launch the timeline
+	
+	if (device == "iPad" || isTablet == true) {
+		launchTimeline();
+	}
+	
+	// Orientation change for tablets.
+
+	$( window ).on( "orientationchange", function( event ) {
+	    windowSize = $(window).width();
+	    checkDeviceType();
+	    if (isTablet == true || device == "iPad") {
+			launchTimeline();
+		}
+	});
+	
+	// Bind the resizing with caveats (Don't do it on a mobile device, or Android tablets)
+	
+	$(window).bind('resize', function(e){
+	    window.resizeEvt;
+	    $(window).resize(function(){
+	        clearTimeout(window.resizeEvt);
+	        window.resizeEvt = setTimeout(function(){
+	        	// Exclude mobile devices.
+		        if(mobileDevice != true) {
+					launchTimeline();
+				}
+	        }, 100);
+	    });
+	});
+
 };
